4cb336fe41c334851d8ca40d07ada4a98bd12a47
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-11 14:07:27 +0200

    Better dark/light mode

--- frontend/src/sections/Navbar.tsx
+++ frontend/src/sections/Navbar.tsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import { Navbar as Nb } from "../stories/sections/navbar/Navbar";
 
 export const Navbar: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const storedMode = localStorage.getItem("data-mode");
+    return storedMode === "light" ? false : true;
+  });
 
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.setAttribute("data-mode", "dark");
+      localStorage.setItem("data-mode", "dark");
     } else {
       document.documentElement.setAttribute("data-mode", "light");
+      localStorage.setItem("data-mode", "light");
     }
   }, [isDarkMode]);
 
