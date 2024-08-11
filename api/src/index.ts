10bcfd9d7773c6be1cd3b1c72fe6e40227388538
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-11 22:10:49 +0200

    Backend init and dockerize

---
+++ api/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+const app = express();
+const port = process.env.BACKEND_PORT;
+const frontendPort = process.env.FRONTEND_PORT;
+const host = process.env.HOST;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: `http://${host}:${frontendPort}`,
+    methods: "GET,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello from backend!");
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
