cac8b0dab074bb675fe8f43e22dda9f77c91f152
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-12 22:33:41 +0200

    Delete handlers note

--- api/src/routes/noteRoutes.ts
+++ api/src/routes/noteRoutes.ts
@@ -4,11 +4,13 @@ import {
   getNoteByIdHandler,
 } from "../handlers/note/getHandlers";
 import { postNoteHandler } from "../handlers/note/postHandlers";
+import { deleteNoteHandler } from "../handlers/note/deleteHandlers";
 
 const router = Router();
 
 router.get("/all", getAllNotesHandler);
 router.get("/id/:id", getNoteByIdHandler);
 router.post("/", postNoteHandler);
+router.delete("/:id", deleteNoteHandler);
 
 export default router;
