ed286033a190124708325cb3e621bf56161285d5
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-12 21:20:47 +0200

    Post Note handler

--- api/src/routes/noteRoutes.ts
+++ api/src/routes/noteRoutes.ts
@@ -3,10 +3,12 @@ import {
   getAllNotesHandler,
   getNoteByIdHandler,
 } from "../handlers/note/getHandlers";
+import { postNoteHandler } from "../handlers/note/postHandlers";
 
 const router = Router();
 
 router.get("/all", getAllNotesHandler);
 router.get("/id/:id", getNoteByIdHandler);
+router.post("/", postNoteHandler);
 
 export default router;
