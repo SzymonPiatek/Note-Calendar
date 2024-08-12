4c6e02610abfa0470de8a18873593408050a0306
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-12 21:32:44 +0200

    Note status post handlers

--- api/src/routes/noteStatusRoutes.ts
+++ api/src/routes/noteStatusRoutes.ts
@@ -3,10 +3,12 @@ import {
   getAllNoteStatusHandler,
   getNoteStatusByIdHandler,
 } from "../handlers/noteStatus/getHandlers";
+import { postNoteStatusHandler } from "../handlers/noteStatus/postHandlers";
 
 const router = Router();
 
 router.get("/all", getAllNoteStatusHandler);
 router.get("/id/:id", getNoteStatusByIdHandler);
+router.post("/", postNoteStatusHandler);
 
 export default router;
