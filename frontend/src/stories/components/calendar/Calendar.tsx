5f0198f23eb859020fee6238659f7b612801221f
Szymon Piątek <szymon.piatek125@gmail.com>
2024-08-12 16:45:29 +0200

    Calendar grid

--- frontend/src/stories/components/calendar/Calendar.tsx
+++ frontend/src/stories/components/calendar/Calendar.tsx
@@ -1,5 +1,19 @@
 import { cva } from "class-variance-authority";
 import clsx from "clsx";
+import React, { useState } from "react";
+import {
+  format,
+  addMonths,
+  subMonths,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+  isSameMonth,
+  isSameDay,
+} from "date-fns";
+import { pl } from "date-fns/locale";
 import { Heading } from "../heading/Heading";
 import { IconButton } from "../button/IconButton";
 
@@ -15,20 +29,60 @@ export const containerVariants = cva([
   "rounded",
 ]);
 
+export const calendarVariants = cva(["grid gap-2"]);
+
 export const Calendar = () => {
   const containerClass = clsx(containerVariants());
+  const calendarClass = clsx(calendarVariants());
+
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const daysOfWeek = ["Pn", "Wt", "Śr", "Cz", "Pt", "Sb", "Nd"];
+
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const handlePrevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+
+  const handleNextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
 
   return (
     <div className={containerClass}>
-      <div className="flex justify-between flex-1 gap-2">
-        <Heading size={2} children="Sierpień" />
+      <div className="flex justify-between gap-2">
+        <Heading
+          size={2}
+          children={capitalizeFirstLetter(
+            format(currentMonth, "LLLL yyyy", { locale: pl })
+          )}
+        />
         <div className="flex gap-2">
-          <IconButton icon="faArrowLeft" size="small" />
-          <IconButton icon="faArrowRight" size="small" />
+          <IconButton
+            icon="faArrowLeft"
+            size="small"
+            onClick={handlePrevMonth}
+          />
+          <IconButton
+            icon="faArrowRight"
+            size="small"
+            onClick={handleNextMonth}
+          />
         </div>
       </div>
       <hr />
-      <div></div>
+      <div className={`calendar ${calendarClass}`}>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+      </div>
     </div>
   );
 };
